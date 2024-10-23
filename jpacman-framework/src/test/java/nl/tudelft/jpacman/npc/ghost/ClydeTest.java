f3ead8344a3c044502b262f5182a5bfd53b88a55
AlineBlg <aline.boulanger@student.unamur.be>
2024-10-23 14:38:10 +0200

    Avancement dans l'exo 3

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -6,11 +6,15 @@ import nl.tudelft.jpacman.sprite.PacManSprites;
 
 public class ClydeTest {
 
+    // Création d'un Clyde
     PacManSprites SPRITE_STORE = new PacManSprites();
     GhostFactory factory = new GhostFactory(SPRITE_STORE);
     Ghost clyde = factory.createClyde();
 
-    
 
+    // variable à tester
+    // On peut def la direction vers laquelle regarde Clyde
+    // Créer notre level, layout (map), ...
+    // En fct de la map, le comportement clyde change
 
 }
