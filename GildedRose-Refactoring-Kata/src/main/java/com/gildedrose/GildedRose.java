e276e894f07f9bbc026ed38e79cb22a7ed98aac5
AlineBlg <aline.boulanger@student.unamur.be>
2024-11-13 14:58:05 +0100

    Résolution des points SonarLint
    Grouper les if et mise en place de constantes

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    static final String AGED = "Aged Brie"
+    static final String BACKSTATGE = "Backstage passes to a TAFKAL80ETC concert"
+    static final String SULFURAS = "Sulfuras, Hand of Ragnaros"
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,28 +12,26 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(AGED)
+                    && !items[i].name.equals(BACKSTATGE)) {
+                if (items[i].quality > 0 && !items[i].name.equals(SULFURAS)) {
                         items[i].quality = items[i].quality - 1;
-                    }
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+
                                 items[i].quality = items[i].quality + 1;
-                            }
+
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+
                                 items[i].quality = items[i].quality + 1;
-                            }
+
                         }
                     }
                 }
@@ -43,13 +44,12 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
+
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
